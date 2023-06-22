e9ac9ef850a1e6b6cd1b337a3de5ac90092ec108
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2023-06-22 18:29:07 +0530

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,22 @@
+FROM node:16.13.1
+
+# Set the working directory inside the container
+WORKDIR /usr/src/app
+
+# Copy package.json and package-lock.json to the working directory
+COPY package*.json ./
+
+# Install app dependencies
+RUN npm install
+
+# Copy the rest of the application code to the working directory
+COPY . .
+
+ENV MONGODB_URI=<your-mongodb-uri>
+
+
+# Expose a port (if your app listens on a specific port)
+EXPOSE 3000
+
+# Start the Node.js application
+CMD [ "npm", "start" ]
---
+++ server.js
@@ -0,0 +1,75 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+const http = require('http').Server(app);
+const io = require('socket.io')(http);
+const mongoose = require('mongoose');
+
+app.use(express.static(__dirname));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+var Message = mongoose.model('Message', { name: String, message: String });
+
+
+const dbUrl =process.env.MONGODB_URI;
+
+mongoose
+  .connect(dbUrl)
+  .then(() => {
+    console.log('MongoDB connected');
+  })
+  .catch((err) => {
+    console.error('MongoDB connection error:', err);
+  });
+
+
+app.get('/messages', (req, res) => {
+  Message.find({})
+    .then((messages) => {
+      res.send(messages);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
+
+app.post('/messages', (req, res) => {
+  var message = new Message(req.body);
+  message.save()
+    .then(() => {
+      io.emit('message', req.body);
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
+
+app.delete('/messages', (req, res) => {
+  Message.deleteMany({})
+    .then(() => {
+      io.emit('message', {}); // Emit an empty message to clear the chat on the client side
+      res.sendStatus(200);
+    })
+    .catch((err) => {
+      console.error(err);
+      res.sendStatus(500);
+    });
+});
+
+
+
+var server = app.listen(3000, () => {
+  console.log('Server is running at port', server.address().port);
+  
+  io.on('connection', (socket) => {
+    console.log('A user is connected');
+  
+    socket.on('disconnect', () => {
+      console.log('A user has disconnected');
+    });
+  });
+});
