89ea3a094ad0282a63d3a852b3d8588feb52b418
Parnish Sharma <90691906+ParnishSharma@users.noreply.github.com>
2023-06-23 14:56:15 +0530

    updated index server

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const bodyParser = require('body-parser');
+const cors = require('cors');
 const app = express();
 const http = require('http').Server(app);
 const io = require('socket.io')(http);
@@ -8,6 +9,7 @@ const mongoose = require('mongoose');
 app.use(express.static(__dirname));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
 
 var Message = mongoose.model('Message', { name: String, message: String });
 
